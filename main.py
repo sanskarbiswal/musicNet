d64df16f48251764cfe49ced8c3445edaa67b4b4
sanskarbiswal <sanskar.biswal2016@vitstudent.ac.in>
2020-04-08 21:46:24 +0530

    Able to Play Music from URL

--- main.py
+++ main.py
@@ -1,23 +1,49 @@
 import os
+import eel
 from time import  sleep
 
 url = "https://www.youtube.com/watch?v=oAvYl_6V8rE"
+
+"""
+Initialize EEL App
+"""
+eel.init('web')
 """
 @define function to generate URL from YouTube Link
 """
+@eel.expose
 def generate_url(url):
+    #print("Generating URL")
     import pafy
-    video = pafy.new(url)
-    best = video.getbestaudio()
-    return best.url
+    music = pafy.new(url)
+    best = music.getbestaudio()
+    metaData = {
+        'title' : music.title,
+        'author': music.author,
+        'thumb' : music.thumb
+    }
+    url = best.url
+    eel.music_url(url, metaData)
 
-def init_player():
-    import vlc
+# @define testing flow of code
+@eel.expose
+def test():
+    print("Testing")
 
-    player = vlc.MediaPlayer(generate_url(url))
-    player.play()
-    print("Started Playing")
+# @define function to smoothly exit when main window is closed
+@eel.expose
+def handle_exit(ar1,ar2):
+    import sys
+    sys.exit(0)
 
-    sleep(10)
-    while player.is_playing():
-        sleep(1)
+
+if __name__ == "__main__":
+    #os.system('cls')
+    size = (1000,700)
+    app_opt = {
+        'mode' : "chrome",
+        'close_callback' : handle_exit
+    }
+    #generate_url(url)
+    eel.start('main.html', options=app_opt, size=size, suppress_error=True)
+    
---
+++ web/js/main.js
@@ -0,0 +1,17 @@
+// Local JS Function
+function playFromURL(){
+    // Get URL from User
+    url = document.getElementById('t_URL').value;
+    console.log(url);
+    eel.generate_url(url);
+    //eel.test();
+}
+// EEL Exposed Functions
+eel.expose(music_url);
+function music_url(url, metaData){
+    // Update Meta Data
+    document.getElementById('thumb_img').src=metaData.thumb;
+    document.getElementById('music_name').innerHTML=metaData.title;
+    // Start Music
+    document.getElementById('audSrc').setAttribute('src', url);
+}
